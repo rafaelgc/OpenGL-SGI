b840fc532f631901bc370c445cbba309ffe1fa69
GONZÁLEZ CARRIZO <ragonca2@dsic.upv.es>
2017-10-26 09:38:12 +0200

    Práctica 3. Mosaico

--- Pract3/pract3.cpp
+++ Pract3/pract3.cpp
@@ -1,25 +1,46 @@
 #include <iostream>
 #include <GL\freeglut.h>
+#include "Utilidades.h"
 
-#define PROYECTO "Estrella de David"
+#define PROYECTO "Mosaico"
 
 using namespace std;
 
-/*
-GLfloat coords[9] = { 0.0, 0.7, 0.0, 0.0, 1.0, 0.0,
--1.0, 0.0, 0.0};
-GLuint indices[3] = { 0, 1, 2 };
-*/
-
 static GLuint triangulo;
+
+void drawTriangle(GLfloat x, GLfloat y, GLfloat angle) {
+	glPushMatrix();
+
+	glTranslatef(x, y, 0.0);
+	glRotatef(angle, 0.0, 0.0, 1.0);
+	glScalef(0.5, 0.5, 1);
+	glCallList(triangulo);
+
+	glRotatef(-angle * 2, 0.0, 0.0, 1.0);
+	glScalef(0.4, 0.4, 1.0);
+	glCallList(triangulo);
+
+	glPopMatrix();
+}
 
 void display() {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glCallList(triangulo);
+	//Superior derecha
+	drawTriangle(0.5, 0.5, -15);
 
+	//Superior izquierda
+	drawTriangle(-0.5, 0.5, 15);
+
+	//Inferior derecha
+	drawTriangle(0.5, -0.5, 15);
+
+	//Inferior izquierda
+	drawTriangle(-0.5, -0.5, -15);
+	
 	glFlush();
+
 }
 
 void reshape(GLint w, GLint h) {
@@ -36,31 +57,31 @@ int main(int argc, char** argv) {
 
 	cout << PROYECTO << " en marcha" << endl;
 
-	glEnableClientState(GL_VERTEX_ARRAY | GL_COLOR_ARRAY);
+	glEnableClientState(GL_VERTEX_ARRAY|GL_COLOR_ARRAY);
 
 	//CALCULAR COORDENADAS
 
 	GLfloat v[12];
 
 	for (unsigned int i = 0; i < 6; i++) {
-		float rad = 3.1415 / 3 + i * 3.1415 / 3; //PI ------------------------
+		float rad = 3.1415 / 3 + i * 3.1415/3; //PI ------------------------
 		float x = sin(rad);
 		float y = cos(rad);
 
 		v[2 * i] = x;
 		v[2 * i + 1] = y;
 	}
-
+	
 	//CONFIGURAR TRIANGULO
 	triangulo = glGenLists(1);
 
 	glNewList(triangulo, GL_COMPILE);
-
+	
 	glColor3f(0.0, 0.0, 0.3);
-
+	
 	glBegin(GL_TRIANGLE_STRIP);
 
-	for (unsigned int i = 0; i < 6; i += 2) {
+	for (unsigned int i = 0; i < 6; i+=2) {
 		//Interior
 		glVertex2f(v[2 * i] * 0.7, v[2 * i + 1] * 0.7);
 
